398024bd14356f51c5c97f2940dea34bc6940011
Caua Rezende <cauagrezende@gmail.com>
2023-05-21 09:47:55 -0300

    feat: add router subpackage

---
+++ main.go
@@ -0,0 +1,8 @@
+package main
+
+import "github.com/rezendecaua/gopportunities/router"
+
+func main() {
+	router.Initialize()
+
+}
---
+++ router/router.go
@@ -0,0 +1,15 @@
+package router
+
+import "github.com/gin-gonic/gin"
+
+func Initialize() {
+	router := gin.Default()
+
+	router.GET("/", func(c *gin.Context) {
+		c.JSON(200, gin.H{
+			"message": "Hello World!",
+		})
+	})
+
+	router.Run(":8080")
+}
